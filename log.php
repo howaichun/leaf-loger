65a5caeb6fbcd5a3aca305edac9d441692acd710
howaichun <hamvy@163.com>
2015-10-12 11:11:39 +0800

    logPHP intro suppose

---
+++ log.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ *  logPHP
+ *
+ *1、   PHPredis
+ *2、   MongoDB
+ *3、   Memcache
+ *4、   文件
+ *5、   基于Mysql的PHP异步日志系统
+ * others log
+ *  java:
+ *   ZOOKeeper
+ *   Openstack
+ *   etc
+ */
+
+
+
+
+
